559df8d574867d25a608e38ba2bcc636ae0443ff
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-04 23:54:20 +0530

    Java Literals and Types

---
+++ src/main/java/JavaLiterals.java
@@ -0,0 +1,14 @@
+public class JavaLiterals {
+    //Literals are data that represents fixed values.They can be used directly in the code.
+    int a =1;
+    char ch = 'F';
+    //here 1 and F are literals
+
+    /** there are 5 types of literals
+     * Boolean Literals -> true or false
+     * Integer Literals -> binary,decimal,hexa, octa
+     * Floating point Literals -> FLoat and Double
+     * Character Literals -> char
+     * String Literal -> String
+     */
+}
