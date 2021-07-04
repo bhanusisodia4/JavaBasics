ccd2db23c80079b3f0a0b42b36426427f0c76f43
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-05 00:02:44 +0530

    Java data types

---
+++ src/main/java/JavaDataTypes.java
@@ -0,0 +1,20 @@
+public class JavaDataTypes {
+    /** As the name suggests it tells us the type of data that can be stored inside a variable
+     */
+
+    public static void main(String[] args) {
+        int i =20; //here int is data type , i is variable name, 20 is literal
+        String name ="bhanu";
+        System.out.println(i+"  "+name);
+    }
+    /** types of data types are:
+     * integer
+     * fLoat
+     * String
+     * double
+     * char
+     * byte
+     * short
+     * long
+     */
+}
