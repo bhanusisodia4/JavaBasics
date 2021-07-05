c8716afb850eafd0b74ee27395a1494526e511f2
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-05 22:12:22 +0530

    Java operators

---
+++ src/main/java/JavaOperators.java
@@ -0,0 +1,19 @@
+public class JavaOperators {
+    /**
+     * Operators are symbols that performs operations on variable and values
+     * Types:->
+     * Arithmatic      * Assignment      * Relational
+     * logical      * Increment /decrement      * Bitwise
+     */
+
+    public static void main(String[] args) {
+        int i= 10;
+        int j= 21;
+        int result;
+        result= i+j; //Addition
+        System.out.println(result);
+
+        result = j-i; //Subtraction
+        System.out.println(result);
+    }
+}
