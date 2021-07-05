42c5e1b61143ff58f56e42af7da77b823a1c9c36
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-06 00:03:02 +0530

    String tokenizer to break String into tokens

---
+++ src/main/java/Tokenizer.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Tokenizer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a string");
+        StringTokenizer st = new StringTokenizer(br.readLine(),"-");
+
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+
+    }
+}
