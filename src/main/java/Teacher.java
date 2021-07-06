61f3d0dae63c98c423a57e41aaf6ae8795eb360a
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-06 18:15:26 +0530

    Abstraction concept program - updated

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,12 +1,19 @@
 abstract class Teacher {
     abstract void eat();
 
+    Teacher(){
+        System.out.println("this is abstract class constructor");
+    }
     public void display(){
         System.out.println("this method is called from abstract class");
     }
 }
 
 class Student extends Teacher{
+    Student(){
+        System.out.println("This is child default constructor");
+    }
+
      void eat(){
          System.out.println(" i eat pizza");
      }
